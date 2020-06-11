fc89e8f739479e80ae04fbe747813367ee890093
jerome74 <walter.longo74@gmail.com>
2020-06-11 09:38:52 +0200

    map

--- app/src/main/java/jonnyb/example/smack/ui/map/MapsActivity.kt
+++ app/src/main/java/jonnyb/example/smack/ui/map/MapsActivity.kt
@@ -1,19 +1,74 @@
 package jonnyb.example.smack.ui.map
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import jonnyb.example.smack.R
+import java.lang.Exception
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    /**
+     * GoogleMap
+     */
+
     private lateinit var mMap: GoogleMap
+
+    /**
+     * LocationManager
+     */
+
+    private lateinit var locationManager : LocationManager
+
+    /**
+     * LocationListener
+     */
+
+    private var locationListener = object :  LocationListener{
+        override fun onLocationChanged(location: Location?) {
+            val myLatLng : LatLng = LatLng(location!!.latitude, location!!.longitude)
+            mMap.addMarker(MarkerOptions().position(myLatLng).title("my Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 15.toFloat()))
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+            try {
+                provider.toString()
+            }catch (e : Exception)
+            {
+                Log.e("ERROR", e.message, e)
+            }
+
+        }
+    }
+
+    /**
+     * onCreate
+     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +77,54 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED)
+
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        else
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0.toFloat(),locationListener)
+
+
+    }
+
+    /**
+     * onRequestPermissionsResult
+     */
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        if(grantResults.isNotEmpty() && grantResults.contains(PackageManager.PERMISSION_GRANTED))
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0.toFloat(),locationListener)
     }
 
     /**
@@ -37,8 +140,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val ciampino = LatLng(41.806396,12.5975753)
+        mMap.addMarker(MarkerOptions().position(ciampino).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ciampino, 15.toFloat()))
     }
+
 }
